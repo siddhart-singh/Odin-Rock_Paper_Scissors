cc03361702b796c802c5d63f690c5c7f88014ff7
siddhart-singh <siddhartsingh@hotmail.com>
2023-09-11 19:46:37 +0530

    Add computer choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+function getComputerChoice() {
+  const computerChoice = ["rock", "paper", "scissor"];
+  return computerChoice[getRandomNumber(1, 3)];
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min + 1);
+}
