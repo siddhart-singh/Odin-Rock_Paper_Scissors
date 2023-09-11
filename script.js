9ca7d16853d66e87c28fe97631868be0b712ae46
siddhart-singh <siddhartsingh@hotmail.com>
2023-09-11 21:00:41 +0530

    Add player input

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 "use strict";
 
-// const result = playRound("rock", getComputerChoice())
-// console.log(result);
+function getPlayerChoice(){
+    
+    let incorrectInput = true;
+    const avaliableChoice = ["rock", "paper", "scissor"];
+    do{
+        let playerChoice = prompt("Rock, Paper or Scissor? ");
+        if(avaliableChoice.includes(playerChoice.toLowerCase())){
+            incorrectInput = false;
+            return playerChoice.toLowerCase();
+        }
+    }while(incorrectInput);
+}
 
 function getComputerChoice() {
   const computerChoice = ["rock", "paper", "scissor"];
-  return computerChoice[getRandomNumber(0, 3)];
+  return computerChoice[getRandomNumber(0, 2)];
 }
 
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function playRound(playerSelection = '', computerSelection = ''){
@@ -23,23 +33,53 @@ function playRound(playerSelection = '', computerSelection = ''){
 function computerResult(playerParity, computerParity, optionsArray){
     if (Math.abs(playerParity - computerParity) != (optionsArray.length - 1)){
         if (playerParity > computerParity){
-            return `You Win! ${optionsArray[playerParity]} beats ${optionsArray[computerParity]}`;
+            return [`You Win! ${optionsArray[playerParity]} beats ${optionsArray[computerParity]}`,1];
         }
 
         else if(computerParity == playerParity){
-            return `Draw! Both picked ${optionsArray[playerParity]}`
+            return [`Draw! Both picked ${optionsArray[playerParity]}`,2];
         }
 
         else{
-            return `You Lose! ${optionsArray[computerParity]} beats ${optionsArray[playerParity]}`;
+            return [`You Lose! ${optionsArray[computerParity]} beats ${optionsArray[playerParity]}`,0];
         }
     }
     else{
         if (playerParity < computerParity){
-            return `You Win! ${optionsArray[playerParity]} beats ${optionsArray[computerParity]}`;
+            return [`You Win! ${optionsArray[playerParity]} beats ${optionsArray[computerParity]}`, 1];
         }
         else{
-            return `You Lose! ${optionsArray[computerParity]} beats ${optionsArray[playerParity]}`;
+            return [`You Lose! ${optionsArray[computerParity]} beats ${optionsArray[playerParity]}`,0];
         }   
     }
 }
+
+function game(){
+    let round  = 5;
+    let playerScore = 0;
+    let computerScore = 0;
+    while(round--){
+        const result = playRound(getPlayerChoice(),getComputerChoice());
+        console.log(result[0])
+        if(result[1] == 0){
+            computerScore++
+        }else if(result[1] == 1){
+            playerScore++;
+        }    
+    }
+    printResult(playerScore, computerScore);
+}
+
+function printResult(playerScore, computerScore){
+    console.log(getResult(playerScore, computerScore));
+}
+
+function getResult(playerScore, computerScore){
+    if(playerScore > computerScore){
+        return "You Won!"
+    } else{
+        return "You Lost!"
+    }
+}
+
+game();
