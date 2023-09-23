c7cc971a4b724aeba557d8da2b0495553858e25e
siddhart-singh <siddhartsingh@hotmail.com>
2023-09-23 23:50:52 +0530

    Add score system

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 "use strict";
 
 let round = 5;
-//Computer Score
 const computerScoreBoard = document.querySelector(
   ".computer-score > .points"
 ).children;
-
-for (let el of computerScoreBoard) {
-  el.classList.add("hidden");
-}
-let currentComputerScore = 0;
-computerScoreBoard[0].classList.remove("hidden");
-
-//PLayer Score
-
+const computerScoreIcon = document.querySelector(".computer-icon").children;
 const playerScoreBoard = document.querySelector(
   ".player-score > .points"
 ).children;
-
-for (let el of playerScoreBoard) {
-  el.classList.add("hidden");
-}
-let currentPlayerScore = 0;
-playerScoreBoard[0].classList.remove("hidden");
-
 const playerChoice = document.querySelector(".player");
-playerChoice.addEventListener("click", (e) => {
-  game(e.target.alt.split("-")[1]);
-});
+const playerScoreIcon = document.querySelector(".player-icon").children;
+let currentComputerScore = 0;
+let currentPlayerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-function getPlayerChoice() {}
+function init() {
+  //Computer Score
+
+  for (let el of computerScoreBoard) {
+    el.classList.add("hidden");
+  }
+  computerScoreBoard[0].classList.remove("hidden");
+
+  for(let el of computerScoreIcon){
+    el.classList.add("hidden");
+  }
+  computerScoreIcon[2].classList.remove("hidden");
+  //PLayer Score
+
+  for (let el of playerScoreBoard) {
+    el.classList.add("hidden");
+  }
+  playerScoreBoard[0].classList.remove("hidden");
+
+  for(let el of playerScoreIcon){
+    el.classList.add("hidden");
+  }
+    playerScoreIcon[2].classList.remove("hidden");
+
+  playerChoice.addEventListener("click", (e) => {
+    if (round > 0) {
+      game(e.target.alt.split("-")[1]);
+    } else {
+      printResult(playerScore, computerScore);
+    }
+  });
+}
+
 function getComputerChoice() {
   const computerChoice = ["rock", "paper", "scissor"];
   return computerChoice[getRandomNumber(0, 2)];
@@ -95,43 +113,108 @@ function printComputeResult(
   computerParity,
   optionsArray
 ) {
-
   if (result === "win") {
     playerScoreBoard[currentPlayerScore].classList.add("hidden");
     playerScoreBoard[++currentPlayerScore].classList.remove("hidden");
-    console.log(`You Win! ${titleCase(optionsArray[playerParity])} beats ${titleCase(optionsArray[computerParity])}`);
+    console.log(
+      `You Win! ${titleCase(optionsArray[playerParity])} beats ${titleCase(
+        optionsArray[computerParity]
+      )}`
+    );
     return 1;
   } else if (result === "lose") {
     computerScoreBoard[currentComputerScore].classList.add("hidden");
     computerScoreBoard[++currentComputerScore].classList.remove("hidden");
-    console.log(`You Lose! ${titleCase(optionsArray[computerParity])} beats ${titleCase(optionsArray[playerParity])}`)
+    console.log(
+      `You Lose! ${titleCase(optionsArray[computerParity])} beats ${titleCase(
+        optionsArray[playerParity]
+      )}`
+    );
     return 0;
   } else {
-    console.log(`Draw! Both picked ${titleCase(optionsArray[playerParity])}`)
+    console.log(`Draw! Both picked ${titleCase(optionsArray[playerParity])}`);
     return 2;
   }
 }
 
-function game(playerChoice) {
-  let playerScore = 0;
-  let computerScore = 0;
-  const result = playRound(playerChoice, getComputerChoice());
-  if (round > 0) {
-    if (result == 0) {
-      computerScore++;
-      round--;
-    } else if (result == 1) {
-      playerScore++;
-      round--;
+function displayScoreIcon(currentPlayerScore, currentComputerScore){
+    if(currentPlayerScore > currentComputerScore){
+        playerScoreIcon[1].classList.add("hidden");
+        playerScoreIcon[2].classList.add("hidden");
+        playerScoreIcon[3].classList.remove("hidden");
+
+        computerScoreIcon[1].classList.remove("hidden");
+        computerScoreIcon[2].classList.add("hidden");
+        computerScoreIcon[3].classList.add("hidden");
+    }else if(currentPlayerScore < currentComputerScore){
+        playerScoreIcon[1].classList.remove("hidden");
+        playerScoreIcon[2].classList.add("hidden");
+        playerScoreIcon[3].classList.add("hidden");
+
+        computerScoreIcon[1].classList.add("hidden");
+        computerScoreIcon[2].classList.add("hidden");
+        computerScoreIcon[3].classList.remove("hidden");
     }
-  } else {
-    printResult(playerScore, computerScore);
+    else{
+        playerScoreIcon[1].classList.add("hidden");
+        playerScoreIcon[2].classList.remove("hidden");
+        playerScoreIcon[3].classList.add("hidden");
+
+        computerScoreIcon[1].classList.add("hidden");
+        computerScoreIcon[2].classList.remove("hidden");
+        computerScoreIcon[3].classList.add("hidden");
+    }
+}
+
+function game(playerChoice) {
+  const result = playRound(playerChoice, getComputerChoice());
+  if (result == 0) {
+    computerScore++;
+    round--;
+  } else if (result == 1) {
+    playerScore++;
+    round--;
   }
+  displayScoreIcon(currentPlayerScore, currentComputerScore);
 }
 
 function printResult(playerScore, computerScore) {
+    if(playerScore > computerScore){
+        for(let el of playerScoreIcon){
+            if(el.dataset.icon == "won"){
+                el.classList.remove("hidden");
+            }else{
+                el.classList.add("hidden");
+            }
+        }
+
+        for(let el of computerScoreIcon){
+            if(eldataset.icon == "lost"){
+                el.classList.remove("hidden");
+            }else{
+                el.classList.add("hidden");
+            }
+        }
+    }else{
+        for(let el of playerScoreIcon){
+            if(el.dataset.icon == "lost"){
+                el.classList.remove("hidden");
+            }else{
+                el.classList.add("hidden");
+            }
+        }
+
+        for(let el of computerScoreIcon){
+            if(el.dataset.icon == "won"){
+                el.classList.remove("hidden");
+            }else{
+                el.classList.add("hidden");
+            }
+        }
+    }
   console.log(getResult(playerScore, computerScore));
 }
+
 
 function getResult(playerScore, computerScore) {
   if (playerScore > computerScore) {
@@ -150,3 +233,5 @@ function titleCase(str = "") {
     return str[0].toUpperCase() + str.slice(1, str.length);
   }
 }
+
+init();
